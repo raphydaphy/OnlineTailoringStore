10bedc16f88aea02fa3b663ee6ee65225d9819e5
raphydaphy <raph.hennessy@gmail.com>
2021-07-16 01:13:13 +1000

    Bootstrap & hot reload

--- src/main/java/com/tailoring/tailoringstore/config/MvcConfiguration.java
+++ src/main/java/com/tailoring/tailoringstore/config/MvcConfiguration.java
@@ -1,23 +1,18 @@
 package com.tailoring.tailoringstore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan
-public class MvcConfiguration extends WebMvcConfigurerAdapter {
-  @Override
-  public void configureViewResolvers(ViewResolverRegistry registry) {
-    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    resolver.setPrefix("/WEB-INF/view/");
-    resolver.setSuffix(".jsp");
-    resolver.setViewClass(JstlView.class);
-    registry.viewResolver(resolver);
+public class MvcConfiguration {
+
+  @Bean
+  public InternalResourceViewResolver resolver() {
+    InternalResourceViewResolver vr = new InternalResourceViewResolver();
+    vr.setPrefix("/WEB-INF/views/");
+    vr.setSuffix(".jsp");
+    return vr;
   }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="webjars/bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+	<title>Online Tailoring Store</title>
+</head>
+<body>
+<div class="container mt-3">
+	<h1>Online Tailoring Store</h1>
+	<p>Welcome to the store</p>
+
+
+	<button type="button" class="btn btn-primary">Log In</button>
+</div>
+
+<script src="webjars/jquery/3.6.0/jquery.min.js"></script>
+<script src="webjars/popper.js/2.5.4/umd/popper.min.js"></script>
+<script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
